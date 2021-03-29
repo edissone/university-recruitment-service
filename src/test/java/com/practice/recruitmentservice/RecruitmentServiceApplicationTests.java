5a53d699a934c4f746054e719574aa0ce7ca049b
Eduard Mihailovskiy <edissone.py@gmail.com>
2021-03-29 21:23:31 +0300

    add travis CI (#1)

--- src/test/java/com/practice/recruitmentservice/RecruitmentServiceApplicationTests.java
+++ src/test/java/com/practice/recruitmentservice/RecruitmentServiceApplicationTests.java
@@ -2,8 +2,11 @@ package com.practice.recruitmentservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@ActiveProfiles(value = "test")
 class RecruitmentServiceApplicationTests {
 
 	@Test
